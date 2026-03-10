205d7fb635d3594ef97b50ee80c9ee4e0026df5f
SowjanyaChilkamari <sowjanyachilkamarii@gmail.com>
2026-03-10 11:10:25 +0530

    Update First.java

--- LAB1/First.java
+++ LAB1/First.java
@@ -1,10 +1,26 @@
+import java.lang.*;
+import java.util.*;
 
-class First{
-   public static void main(String[] args){
-        int x=10;
-	double y=x;
-	float z=(float)y;
-	long num=400;
-	int a=(int)num;
+public class JavaVariables {
+
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 20;
+        float num3 = (int) num1;
+        char ch = 'A';
+        boolean variable = true;
+
+        if (num1 > num2) {
+            variable = false;
+        }
+
+        String str = "Java Programming";
+        String str1 = new String("Java Programming");
+
+        System.out.println(num1 + " " + num2 + " " + num3);
+        System.out.println(str1 == str);
+        System.out.println(str1 + ", " + str);
+        System.out.println(variable);
     }
 }
