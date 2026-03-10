2e31af6a2f5d87c6c0f442d9a1e02183536834df
SowjanyaChilkamari <sowjanyachilkamarii@gmail.com>
2026-03-10 11:13:34 +0530

    Update Third.java

--- LAB1/Third.java
+++ LAB1/Third.java
@@ -1,25 +1,9 @@
-import java.util.Scanner;
-class ForEachLoop{
-	public static void main(String[] args){
-	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter the number of elements: ");
-	int n=sc.nextInt();
-	int[] arr=new int[n];
-	for(int i=0;i<n;i++){
-		arr[i]=sc.nextInt();
-	}
-	int count=0;int countn=0;
-	for(int num: arr){
-		 count=0;
-		for(int i=1;i<=num;i++){
-			if (num%i==0)
-				count++;
-		}
-		if (count==2){
-			System.out.print(num+" ");
-			countn++;
-		}
-      }
-      System.out.println("\n"+countn);
-  }
-} 
+ class First{
+   public static void main(String[] args){
+        int x=10;
+	double y=x;
+	float z=(float)y;
+	long num=400;
+	int a=(int)num;
+    }
+ }
