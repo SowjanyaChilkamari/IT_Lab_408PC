49cd2b9b165ae11f07b8329fe8272b0aae13f624
SowjanyaChilkamari <sowjanyachilkamarii@gmail.com>
2026-03-10 11:12:11 +0530

    Update Second.java

--- LAB1/Second.java
+++ LAB1/Second.java
@@ -1,24 +1,9 @@
-import java.util.Scanner;
-class Main1{
-	public static void main(String[] args){
-	Scanner sc=new Scanner(System.in);int n=sc.nextInt();
-        int choice=0;
-	int i=1;
-	Outerwhile: while(i<=n){ 
-	               for(int j=1;j<=10;j++){
-			System.out.println(i+"x"+j+"="+i*j);
-			if (j>=10){
-			System.out.println("If you want to continue please enter 1 otherwise 0");
-			choice=sc.nextInt();
-			switch(choice){
-				case 0: break Outerwhile;
-				case 1: continue Outerwhile;
-				}
-			}
-			i++;
-        
+import java.lang.*;
 
-			}
-		}
-	}
+public class FirstProgram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Java Programming!");
+    }
 }
